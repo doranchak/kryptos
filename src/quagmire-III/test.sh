854f0509241542a05348f93e64f024570d7b914e
david.x.oranchak <david.oranchak@jacobs.com>
2023-08-14 15:46:36 -0400

    added K1 and K2 to test

--- src/quagmire-III/test.sh
+++ src/quagmire-III/test.sh
@@ -1,5 +1,9 @@
 make
 
+
+echo -------------------------------------------------------------------------------------------
+echo Example from https://www.cryptogram.org/downloads/aca.info/ciphers/QuagmireIII.pdf
+echo -------------------------------------------------------------------------------------------
 echo Expected ciphertext: 
 echo KRSLWMITJDVIABMRGQMTMLLIVIFUIXRHTNYONVRHHIIIRMCAOVEI
 echo Actual ciphertext:
@@ -7,5 +11,33 @@ echo Actual ciphertext:
 
 echo Expected plaintext: 
 echo THESAMEKEYEDALPHABETISUSEDFORPLAINANDCIPHERALPHABETS
-echo Actual ciphertext:
+echo Actual plaintext:
 ./decrypt test-decrypt.txt AUTOMOBILE HIGHWAY
+
+echo
+echo -------------------------------------------------------------------------------------------
+echo K1
+echo -------------------------------------------------------------------------------------------
+echo Expected ciphertext: 
+echo EMUFPHZLRFAXYUSDJKZLDKRNSHGNFIVJYQTQUXQBQVYUVLLTREVJYQTMKYRDMFD
+echo Actual ciphertext:
+./encrypt test-K1-pt.txt KRYPTOS PALIMPSEST
+
+echo Expected plaintext: 
+echo BETWEENSUBTLESHADINGANDTHEABSENCEOFLIGHTLIESTHENUANCEOFIQLUSION
+echo Actual plaintext:
+./decrypt ../../ciphers/kryptos/K1.txt KRYPTOS PALIMPSEST
+
+echo
+echo -------------------------------------------------------------------------------------------
+echo K2
+echo -------------------------------------------------------------------------------------------
+echo Expected ciphertext: 
+echo VFPJUDEEHZWETZYVGWHKKQETGFQJNCEGGWHKKDQMCPFQZDQMMIAGPFXHQRLGTIMVMZJANQLVKQEDAGDVFRPJUNGEUNAQZGZLECGYUXUEENJTBJLBQCRTBJDFHRRYIZETKZEMVDUFKSJHKFWHKUWQLSZFTIHHDDDUVH?DWKBFUFPWNTDFIYCUQZEREEVLDKFEZMOQQJLTTUGSYQPFEUNLAVIDXFLGGTEZ?FKZBSFDQVGOGIPUFXHHDRKFFHQNTGPUAECNUVPDJMQCLQUMUNEDFQELZZVRRGKFFVOEEXBDMVPNFQXEZLGREDNQFMPNZGLFLPMRJQYALMGNUVPDXVKPDQUMEBEDMHDAFMJGZNUPLGEWJLLAETG
+echo Actual ciphertext:
+./encrypt test-K2-pt.txt KRYPTOS ABSCISSA
+
+echo Expected plaintext: 
+echo ITWASTOTALLYINVISIBLEHOWSTHATPOSSIBLETHEYUSEDTHEEARTHSMAGNETICFIELDXTHEINFORMATIONWASGATHEREDANDTRANSMITTEDUNDERGRUUNDTOANUNKNOWNLOCATIONXDOESLANGLEYKNOWABOUTTHISTHEYSHOULDITSBURIEDOUTTHERESOMEWHEREXWHOKNOWSTHEEXACTLOCATIONONLYWWTHISWASHISLASTMESSAGEXTHIRTYEIGHTDEGREESFIFTYSEVENMINUTESSIXPOINTFIVESECONDSNORTHSEVENTYSEVENDEGREESEIGHTMINUTESFORTYFOURSECONDSWESTIDBYROWS
+echo Actual ciphertext:
+./decrypt ../../ciphers/kryptos/K2.txt KRYPTOS ABSCISSA
